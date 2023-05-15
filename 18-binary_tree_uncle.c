255c8b7c7af89c131095a0da35ce0d16f1e483b9
Cristian Encalada Camargo <cris.encalada.camargo@gmail.com>
2023-05-15 14:07:32 -0300

    Task 18. Fixed segmentation fault (NULL case)

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,10 +9,10 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *grandparent = node->parent->parent;
-
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
+
+	binary_tree_t *grandparent = node->parent->parent;
 
 	if (grandparent->left == node->parent)
 		return (grandparent->right);
