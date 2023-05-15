14862bb3645973b616d0c3809006bdb12d61e794
Cristian Encalada Camargo <cris.encalada.camargo@gmail.com>
2023-05-15 14:14:48 -0300

    Task 18. Fixed compilation warning about ISO C90 mixed declarations

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,10 +9,12 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *grandparent;
+
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *grandparent = node->parent->parent;
+	grandparent = node->parent->parent;
 
 	if (grandparent->left == node->parent)
 		return (grandparent->right);
